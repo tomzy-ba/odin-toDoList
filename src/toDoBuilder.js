eec7a1808db6a441ab036a477dfd77aa6b75f41d
tomzy-ba <trbee100@gmail.com>
2024-02-10 21:05:54 +0000

    Add header and navigation to index.html

---
+++ src/toDoBuilder.js
@@ -0,0 +1,11 @@
+class ToDo {
+    constructor(title, summary, dueDate, priority, status) {
+        this.title = title;
+        this.summary = summary;
+        this.dueDate = dueDate;
+        this.priority = priority
+        this.status = status;
+    }
+}
+
+
