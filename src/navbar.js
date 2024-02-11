114e4ec267b2eef90fe9ad6dfbef85144d45aa83
tomzy-ba <trbee100@gmail.com>
2024-02-11 18:43:22 +0000

    Update navbar styling and add buttons for different pages

--- src/navbar.js
+++ src/navbar.js
@@ -1,3 +1,10 @@
 export function navbar() {
-   const homePage = document.querySelector() 
+   const homePage = document.querySelector("#homeButton");
+   const easyPage = document.querySelector("#easyButton");
+   const moderatePage = document.querySelector("#moderateButton");
+   const hardPage = document.querySelector("#hardButton");
+   const addPage = document.querySelector("#addButton")
+
+
+   
 }
