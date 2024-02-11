adc143fa23e2df4f66730e88a9a9297cdca4626c
tomzy-ba <trbee100@gmail.com>
2024-02-11 19:41:21 +0000

    Add event listeners and input fields for creating a new ToDo item

--- src/index.js
+++ src/index.js
@@ -13,6 +13,17 @@ const dialog = document.querySelector("#dialog")
 const submitDialog = document.querySelector("#submitDialog");
 const closeDialog = document.querySelector("#closeDialog");
 
-addBtn.addEventListener("click", () =>{
+const title = document.querySelector("#titleInput");
+const summary = document.querySelector("#summaryInput");
+const dueDate = document.querySelector("#dueDateInput");
+const priority = document.querySelector("#priorityInput");
+const status = document.querySelector("#statusInput");
+const difficulty = document.querySelector("#difficultyInput");
+
+addBtn.addEventListener("click", () => {
     dialog.showModal();
+})
+submitDialog.addEventListener("click", () => {
+    new ToDo(title.value, summary.value, dueDate.value, priority.value, status.value, difficulty.value)
+    toDoDisplay(toDoArray)
 })
