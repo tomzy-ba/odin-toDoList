872af5ecf9207775b46279a9705a4d1f3d51cd05
tomzy-ba <trbee100@gmail.com>
2024-02-12 19:30:41 +0000

    Add export keyword to ToDoClass and update mainArray

--- src/toDoClass.js
+++ src/toDoClass.js
@@ -3,13 +3,16 @@ let easyArray = [];
 let moderateArray = [];
 let hardArray = [];
 
-class ToDoClass{
+export class ToDoClass{
     constructor(title, summary, dueDate, status, difficulty) {
         this.title = title;
         this.summary = summary;
         this.dueDate = dueDate;
         this.status = status;
         this.difficulty = difficulty;
+
+
+        mainArray.push(this);
         if (difficulty == "easy") {easyArray.push(this)};
         if (difficulty == "moderate") {moderateArray.push(this)};
         if (difficulty == "hard") {hardArray.push(this)};
